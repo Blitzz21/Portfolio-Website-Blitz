f7aafa720605edcb1340a2b8b68d2fbba9a02763
Blitz <johnfloydmarticio@proton.me>
2024-04-27 19:24:52 +0800

    fixed the buttons and added some small changes and made greetings responsive

--- src/components/Greetings/Greetings.jsx
+++ src/components/Greetings/Greetings.jsx
@@ -13,17 +13,17 @@ const Greetings = () => {
   return (
   <div id='Home' className='Greetings'>
     <div data-aos="fade" className='p-container'>
-      <p className='Greetings__first-p'>Hi, I'm John Floyd Marticio</p>
+      <p className='Greetings__first-p'>Hi, I'm John Floyd Marticio😄</p>
       <p className='Greetings__second-p'>I'm a, <span>Freelance Web Developer</span>.</p>
-    </div>
 
-    <div className='buttons-container'>
-      <div className='Greetings__contact-button'>
-        <button data-aos="zoom-in" type="button">Contact Me</button>
-      </div>
-      <div data-aos="zoom-in" className='Greetings__download-cv'>
-        <a href='CV.docx' download>Download CV</a>
-      </div>
+        <div className='buttons-container'>
+          <div className='Greetings__contact-button'>
+            <button data-aos="zoom-in" type="button">Contact Me</button>
+          </div>
+          <div data-aos="zoom-in" className='Greetings__download-cv'>
+            <a href='CV.docx' download>Download CV</a>
+          </div>
+        </div>
     </div>
   </div>
   )
